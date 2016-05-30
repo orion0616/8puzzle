26ca4554e23deca585899968cbbfbe5cbf8c8d19
orion46 <earth.nobu.light@gmail.com>
2016-05-30 14:48:41 +0900

    Remove BinaryHeap.cc
    template class can't be implemented in source file.
    BinaryHeap's implementation in BinaryHeap.h

--- BinaryHeap.h
+++ BinaryHeap.h
@@ -10,8 +10,8 @@ class BinaryHeap {
 public:
 	int n;
 	std::vector<T> a;
-	virtual void bubbleUp(int i)= 0;
-	virtual void trickleDown(int i)=0;
+	virtual void bubbleUp(int i);
+	virtual void trickleDown(int i);
 	bool empty(){
 		return n==0;
 	}
@@ -27,8 +27,8 @@ public:
 	std::unordered_map<int,int> index;
 	BinaryHeap(){n=0;}
 	virtual ~BinaryHeap(){;}
-	virtual bool add(T x)=0;
-	virtual T remove()=0;
+	virtual bool add(T x);
+	virtual T remove();
 	T findMin() {
 		return a[0];
 	}
@@ -36,6 +36,62 @@ public:
 		return n;
 	}
 };
+
+template<class T>
+void BinaryHeap<T>::bubbleUp(int i){
+        int p = parent(i);
+        while (i > 0 && (a[i] < a[p])) {
+                T tmp = a[i];
+                a[i] = a[p];
+                a[p] = tmp;
+                i = p;
+                p = parent(i);
+        }
+}
+
+template<class T>
+void BinaryHeap<T>::trickleDown(int i) {
+        do {
+                int j = -1;
+                int r = right(i);
+                if (r < n && (a[r] < a[i])) {
+                        int l = left(i);
+                        if (a[l] < a[r]) {
+                                j = l;
+                        } else {
+                                j = r;
+                        }
+                } else {
+                        int l = left(i);
+                        if (l < n && (a[l] < a[i])) {
+                                j = l;
+                        }
+                }
+                if (j >= 0){
+                        T tmp = a[i];
+                        a[i] = a[j];
+                        a[j] = tmp;
+                }
+                i = j;
+        } while (i >= 0);
+}
+
+template<class T>
+bool BinaryHeap<T>::add(T x){
+        a.push_back(x);
+        n++;
+        bubbleUp(n-1);
+        return true;
+}
+
+template<class T>
+T BinaryHeap<T>::remove(){
+        T x = a[0];
+        a[0] = a[--n];
+        a.pop_back();
+        trickleDown(0);
+        return x;
+}
 
 
 #endif /* BINARYHEAP_H_ */
--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
-8puzzle: 8puzzle.o BinaryHeap.o NodeBinaryHeap.o Node.o
-	g++ -Wall -O2 8puzzle.o NodeBinaryHeap.o Node.o
+8puzzle: 8puzzle.o NodeBinaryHeap.o Node.o
+	g++ -Wall -O2  8puzzle.o NodeBinaryHeap.o  Node.o
+
 8puzzle.o: 8puzzle.cc
 	g++ -c 8puzzle.cc
-
-BinaryHeap.o: BinaryHeap.cc
-	g++ -c BinaryHeap.cc
 
 NodeBinaryHeap.o: NodeBinaryHeap.cc
 	g++ -c NodeBinaryHeap.cc
@@ -12,5 +10,5 @@ NodeBinaryHeap.o: NodeBinaryHeap.cc
 Node.o: Node.cc
 	g++ -c Node.cc
 
-clear:
+clean:
 	rm -rf *.o
